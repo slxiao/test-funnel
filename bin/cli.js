d6ba0bded3a822bb99586bb856002b6c5d7a02c9
Shelwin Xiao <562687381@qq.com>
2019-07-23 15:49:00 +0800

    split files and add cli api

--- bin/cli.js
+++ bin/cli.js
@@ -1,14 +1,73 @@
 #!/usr/bin/env node
 
-import proces from 'process';
+#!/usr/bin/env node
 
+'use strict';
+
+const process = require('process');
 const program = require('commander');
+const rc = require('rc')('funnel');
 const version = require('../package.json').version;
+const chalk = require('chalk');
 
 program
-  .version(version)
-  .usage('[options] <src...>')
-  .option('--debug', 'turn on debug output', false)
-  .parse(process.argv);
+	.version(version)
+	.usage('[options]')
+	.option('-t, --testfolder <path>', 'folder of test files')
+	.parse(process.argv);
 
-console.log("hello")
+const funnel = require('../lib/api');
+const config = Object.assign({}, rc);
+
+program.options.forEach((opt) => {
+	const name = opt.name();
+
+	if (program[name]) {
+		config[name] = program[name];
+	}
+});
+
+let exitCode = 0;
+
+delete config._;
+delete config.config;
+delete config.configs;
+
+if (rc.config) {
+	log('using runtime config %s', rc.config);
+}
+
+if (program.testfolder) {
+	config.testFolder = program.testfolder;
+}
+
+new Promise((resolve, reject) => {
+	if (program.stdin) {
+		let buffer = '';
+
+		process.stdin
+			.resume()
+			.setEncoding('utf8')
+			.on('data', (chunk) => {
+				buffer += chunk;
+			})
+			.on('end', () => {
+				try {
+					resolve(JSON.parse(buffer));
+				} catch (e) {
+					reject(e);
+				}
+			});
+	} else {
+		resolve(program.args);
+	}
+})
+	.then(() => {
+		return funnel(config);
+	})
+	.catch((err) => {d
+		console.log('\n%s %s\n', chalk.red('✖'), err.stack);
+		process.exit(1);
+	});
+
+
