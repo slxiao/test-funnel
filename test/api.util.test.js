2a439562b73d7b6874284166196ceac364162dd9
Shelwin Xiao <562687381@qq.com>
2019-07-23 14:09:51 +0800

    add test cases

--- test/api.util.test.js
+++ test/api.util.test.js
@@ -1,7 +1,27 @@
 'use strict';
 
-const parse = require('../lib/util')
+import  { parse } from '../lib/util.js'
 
-test('Parse int should be ok', () => {
+test('Parse int variable should be ok', () => {
     expect(parse("%s", 1)).toBe("1");
+  });
+
+test('Parse string variable should be ok', () => {
+    expect(parse("%s", "a")).toBe("a");
+  });
+
+test('Parse multi variables should be ok', () => {
+    expect(parse("%s%s", "a", 1)).toBe("a1");
+  });
+
+test('Overlap arrays should return true', () => {
+    expect(twoArrayOverlap([1], [1])).toBeTruthy();
+  });
+
+test('NotOverlap arrays should return false', () => {
+    expect(twoArrayOverlap([1], [0])).toBeFalsy();
+  });
+
+test('twoArrayOverlap should work for empty arrays', () => {
+    expect(twoArrayOverlap([], [])).toBeFalsy();
   });
